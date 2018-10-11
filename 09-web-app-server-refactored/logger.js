22508d5911a2d11df124232c4d277907faec807d
Magesh <tkmagesh77@gmail.com>
2018-10-11 15:00:00 +0530

    using npm packages (chalk & nodemon)

--- 09-web-app-server-refactored/logger.js
+++ 09-web-app-server-refactored/logger.js
@@ -1,9 +1,10 @@
+var chalk = require('chalk');
 module.exports = function(req, res, next){
 	var startTime = new Date();
 	res.on('finish', function(){
 		var endTime = new Date(),
 			elapsedTime = endTime - startTime;
-		var logMessage = req.method + '\t' + req.urlObj.pathname + '\t' + res.statusCode + ' ' + elapsedTime + 'ms';
+		var logMessage = chalk.red(req.method) + '\t' + chalk.green(req.urlObj.pathname) + '\t' + res.statusCode + ' ' + chalk.inverse(elapsedTime + ':ms');
 		console.log(logMessage);
 	});
 	next();
