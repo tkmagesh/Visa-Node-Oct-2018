965a83f66323e176151b54d1405afffbaec62250
Magesh <tkmagesh77@gmail.com>
2018-10-12 12:24:16 +0530

    using bluebird

--- 11-task-manager-promise/data/taskDb.js
+++ 11-task-manager-promise/data/taskDb.js
@@ -3,7 +3,7 @@ var fs = require('fs'),
 
 
 var dbFile = path.join(__dirname, 'tasks.json');
-
+/*
 module.exports = {
 
 	getData(){
@@ -31,5 +31,41 @@ module.exports = {
 			});
 		});
 
+	}
+}*/
+
+//using util.promisify
+/*var util = require('util'),
+	readFile = util.promisify(fs.readFile),
+	writeFile = util.promisify(fs.writeFile);
+
+module.exports = {
+	getData(){
+		return readFile(dbFile, 'utf8')
+			.then(function(rawData){
+				return JSON.parse(rawData);
+			});
+	},
+	saveData(taskList){
+		var rawData = JSON.stringify(taskList);
+		return writeFile(dbFile, rawData);
+	}
+}*/
+
+//using bluebird
+var bluebird = require('bluebird');
+
+bluebird.promisifyAll(fs);
+
+module.exports = {
+	getData(){
+		return fs.readFileAsync(dbFile, 'utf8')
+			.then(function(rawData){
+				return JSON.parse(rawData);
+			});
+	},
+	saveData(taskList){
+		var rawData = JSON.stringify(taskList);
+		return fs.writeFileAsync(dbFile, rawData);
 	}
 }
